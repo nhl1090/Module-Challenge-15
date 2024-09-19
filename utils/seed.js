f9ce6f5fde5c3659f0940b47dacc8faba0debdbc
nhl1090 <neilhdz24@gmail.com>
2024-09-19 14:17:41 -0400

    Boiler plate code for utils/seed.js

---
+++ utils/seed.js
@@ -0,0 +1,38 @@
+const connection = require('../config/connection');
+const { User, Thoughts } = require('../models');
+const { getRandomName, getRandomThoughts } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  // Delete the collections if they exist
+  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection('thoughts');
+  }
+  
+  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+  if (userCheck.length) {
+    await connection.dropCollection('users');
+  }
+
+  const users = [];
+  const thoughts = getRandomThoughts(10);
+
+  for (let i = 0; i < 20; i++) {
+    const fullName = getRandomName();
+    const first = fullName.split(' ')[0];
+    const last = fullName.split(' ')[1];
+
+    users.push({
+      first,
+      last,
+      age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
+    });
+  }
+
+  await User.insertMany(users);
+  await Thoughts.insertMany(thoughts);
+
+});
